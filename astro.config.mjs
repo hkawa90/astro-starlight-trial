7467360e69743e3567b769b17d3340bf9ca87f6e
Hideo Kawamura <hideo.90.kawamura@gmail.com>
2024-10-05 23:33:06 +0900

    change site configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightObsidian, { obsidianSidebarGroup } from 'starlight-obsidian';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://hkawa90.github.io/astro-starlight-trial',
+	outDir: './docs', // for github pages
 	integrations: [
 		starlight({
+			plugins: [
+				// Generate the Obsidian vault pages.
+				starlightObsidian({
+					vault: '/mnt/c/Users/hideo/Documents/Obsidian/MyDoc/',
+				}),
+                        ],
 			title: 'My Docs',
+                        // pagefindで日本語で検索できるようにするためlocaleを設定する
+			locales: {
+                                 root: {
+					label: 'Japanese',
+					lang: 'ja',
+				},
+			},
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
@@ -22,6 +38,8 @@ export default defineConfig({
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
 				},
+				// Add the generated sidebar group to the sidebar.
+				obsidianSidebarGroup,
 			],
 		}),
 	],
